f40b07b1017e339a7d1c046745fe12dc5c073e9d
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-01 21:41:43 -0300

    feat: add seed for curso and aluno

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,29 +1,35 @@
-import prisma from "../src/prisma";
+import prisma from '../src/prisma';
 // import * as faker from 'faker-br';
 
-const length = 50
+const length = 50;
 
 const alunos = Array.from({ length }).map((v, i) => ({
   matricula: i,
-}))
-
-// const interests = Array.from({ length }).map((v, i) => ({
-//   uConfirmation: i < mean ? false : true,
-//   pConfirmation: i < mean ? false : true,
-//   userId: length - i,
-//   propertyId: i + 1
-// }))
+  cursoId: i,
+}));
+const curso = Array.from({ length }).map((v, i) => ({
+  id: i,
+  nome: 'Curso ' + i,
+  codigo: i,
+}));
 
 async function main() {
+  await prisma.aluno.deleteMany({});
+  await prisma.curso.deleteMany({});
+
+  await prisma.curso.createMany({
+    data: curso,
+  });
   await prisma.aluno.createMany({
-    data: alunos
-  })
+    data: alunos,
+  });
 }
 
-main().catch((e) => {
-  console.log(e)
-  process.exit(1)
-}).finally(() => {
-  prisma.$disconnect()
-})
-
+main()
+  .catch(e => {
+    console.log(e);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
