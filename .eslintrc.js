1868cedae160d536843764104e4dabc976a90f50
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-01 21:38:46 -0300

    chore: improve eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-    'prettier'
-  ],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
+    'prettier/prettier': 'error',
   },
 };
