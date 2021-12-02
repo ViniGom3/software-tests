3d8a9f05667fed548dc23bccfacd13b732edfe63
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-01 22:54:16 -0300

    feat: add new routes and rn1 and rn2

--- src/routes/alunos.ts
+++ src/routes/alunos.ts
@@ -13,7 +13,7 @@ export const verify = async function (hash: string, plaintext: string) {
 
 const router = Router();
 
-router.get('/users', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const allAlunos = await prisma.aluno.findMany({
       select: {
@@ -21,9 +21,156 @@ router.get('/users', async (req, res, next) => {
       },
     });
 
-    throw new Exception(400, 'fodfe');
+    res.json(allAlunos);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { matricula } = req.body;
+
+    const aluno = await prisma.aluno.create({
+      data: {
+        matricula: matricula,
+      },
+    });
+
+    res.json(aluno);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/', async (req, res, next) => {
+  try {
+    const { matricula } = req.body;
+
+    const aluno = await prisma.aluno.delete({
+      where: {
+        matricula: matricula,
+      },
+    });
+
+    res.json(aluno);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch('/', async (req, res, next) => {
+  try {
+    const { matricula, cursoId } = req.body;
+
+    const aluno = await prisma.aluno.update({
+      where: {
+        matricula: matricula,
+      },
+      data: {
+        cursoId: cursoId,
+      },
+    });
+
+    res.json(aluno);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/', async (req, res, next) => {
+  try {
+    const allAlunos = await prisma.aluno.findMany({
+      select: {
+        matricula: true,
+      },
+    });
 
     res.json(allAlunos);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:matricula/ira', async (req, res, next) => {
+  try {
+    const { matricula } = req.params;
+    const matriculaAluno = Number.parseInt(matricula);
+
+    const avaliacao = await prisma.avaliacao.findMany({
+      where: {
+        matriculaAluno: matriculaAluno,
+      },
+      select: {
+        grauFinal: true,
+        turma: {
+          select: {
+            Disciplina: {
+              select: {
+                cargaHoraria: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const sumAllExames = avaliacao.reduce(
+      (acc, value) =>
+        acc + value.grauFinal * value.turma.Disciplina.cargaHoraria,
+      0,
+    );
+    const sumAllHour = avaliacao.reduce(
+      (acc, value) => acc + value.turma.Disciplina.cargaHoraria,
+      0,
+    );
+
+    res.json({ IRA: sumAllExames / sumAllHour });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:matricula/:id_periodo/ira', async (req, res, next) => {
+  try {
+    const { matricula, id_periodo } = req.params;
+    const matriculaAluno = Number.parseInt(matricula);
+    const periodId = Number.parseInt(id_periodo);
+
+    const avaliacao = await prisma.avaliacao.findMany({
+      where: {
+        matriculaAluno: matriculaAluno,
+        turma: {
+          periodoLetivo: {
+            id: periodId,
+          },
+        },
+      },
+      select: {
+        grauFinal: true,
+        turma: {
+          select: {
+            Disciplina: {
+              select: {
+                cargaHoraria: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const sumAllExames = avaliacao.reduce(
+      (acc, value) =>
+        acc + value.grauFinal * value.turma.Disciplina.cargaHoraria,
+      0,
+    );
+    const sumAllHour = avaliacao.reduce(
+      (acc, value) => acc + value.turma.Disciplina.cargaHoraria,
+      0,
+    );
+
+    res.json({ IRA: sumAllExames / sumAllHour });
   } catch (error) {
     next(error);
   }
