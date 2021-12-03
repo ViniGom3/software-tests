8f40fdb8d949d86aba23cd6d5f42c5dc5e1a3052
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-02 23:34:31 -0300

    feat: get all disciplina

--- src/controller/disciplina.ts
+++ src/controller/disciplina.ts
@@ -1,5 +1,16 @@
 import { Router } from 'express';
+import prisma from '../prisma';
 
 const router = Router();
+
+router.get('/', async (_, res, next) => {
+  try {
+    const allDisciplinas = await prisma.disciplina.findMany();
+
+    res.json(allDisciplinas);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
