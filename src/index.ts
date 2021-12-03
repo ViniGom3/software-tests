170e8f478c84ebb5cf30d095265e3873696940f1
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-02 22:37:23 -0300

    chore: change folder name

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { setupMiddlewares } from './middlewares';
-import { setupRoutes } from './routes';
+import { setupRoutes } from './controller';
 
 export const app = express();
 setupMiddlewares(app);
