2342c3e0caa426b6e483ab4855caf7414ba4cea1
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-02 22:54:00 -0300

    feat: add more helper functions to aluno service

--- src/services/alunos.ts
+++ src/services/alunos.ts
@@ -1,21 +1,29 @@
-import { Avaliacao, Disciplina } from ".prisma/client";
+import { Avaliacao, Disciplina } from '.prisma/client';
+import argon2 from 'argon2';
 
-export type AvaliacaoService = Pick<Avaliacao, 'grauFinal'> & {   
-    turma: {
-        Disciplina: Pick<Disciplina, 'cargaHoraria'>
-    }
-}
+export type AvaliacaoService = Pick<Avaliacao, 'grauFinal'> & {
+  turma: {
+    Disciplina: Pick<Disciplina, 'cargaHoraria'>;
+  };
+};
 
 export const calcularIra = (avaliacao: AvaliacaoService[]) => {
-    const sumAllExames = avaliacao.reduce(
-        (acc, value) =>
-          acc + value.grauFinal * value.turma.Disciplina.cargaHoraria,
-        0,
-      );
-      const sumAllHour = avaliacao.reduce(
-        (acc, value) => acc + value.turma.Disciplina.cargaHoraria,
-        0,
-      );
-    
-      return sumAllExames / sumAllHour
-}
+  const sumAllExames = avaliacao.reduce(
+    (acc, value) => acc + value.grauFinal * value.turma.Disciplina.cargaHoraria,
+    0,
+  );
+  const sumAllHour = avaliacao.reduce(
+    (acc, value) => acc + value.turma.Disciplina.cargaHoraria,
+    0,
+  );
+
+  return sumAllExames / sumAllHour;
+};
+
+export const hashing = async function (value: string) {
+  return await argon2.hash(value);
+};
+
+export const verify = async function (hash: string, plaintext: string) {
+  return await argon2.verify(hash, plaintext);
+};
