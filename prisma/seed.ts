ba4c1152d962d63a793f5897513409ca5d693b4e
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-04 17:32:33 -0300

    chore: remove set id in seed archive

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,26 +4,22 @@ import prisma from '../src/prisma';
 
 const length = 50;
 
-const alunos = Array.from({ length }).map((v, i) => ({
-  matricula: i,
+const alunos = Array.from({ length }).map((_, i) => ({
   cursoId: i,
 }));
-const curso = Array.from({ length }).map((v, i) => ({
-  id: i,
+const curso = Array.from({ length }).map((_, i) => ({
   nome: 'Curso ' + i,
   codigo: i,
 }));
 
-const turma = Array.from({ length: 2 }).map((v, i) => ({
-  codigo: i,
+const turma = Array.from({ length: 2 }).map((_, i) => ({
   nomeProfessor: 'Professor ' + i,
   periodoLetivoId: i % 2 === 0 ? 1 : 2,
   horario: '08:00 - 18:00',
   disciplinaId: 0,
 }));
 
-const avaliacao = Array.from({ length: 200 }).map((v, i) => ({
-  id: i,
+const avaliacao = Array.from({ length: 200 }).map((_, i) => ({
   matriculaAluno: Math.floor(i / 4),
   codigoTurma: i % 2 === 0 ? 0 : 1,
   grauFinal: Math.floor(Math.random() * 10),
@@ -45,7 +41,6 @@ async function main() {
   });
   await prisma.periodoLetivo.create({
     data: {
-      id: 1,
       dataInicio: new Date('2021-01-01T08:00:00'),
       dataFim: new Date('2021-06-30T23:59:59'),
       status: 'ATIVO',
@@ -53,7 +48,6 @@ async function main() {
   });
   await prisma.periodoLetivo.create({
     data: {
-      id: 2,
       dataInicio: new Date('2021-01-01T08:00:00'),
       dataFim: new Date('2021-06-30T23:59:59'),
       status: 'ATIVO',
@@ -61,7 +55,6 @@ async function main() {
   });
   await prisma.disciplina.create({
     data: {
-      codigo: 0,
       nome: 'Topicos Especiais em Sistemas de Informação',
       cargaHoraria: 60,
       nomeDepartamento: 'Departamento de Computação',
