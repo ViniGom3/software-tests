7311523731062f785621a5cf0888f898ce5d8c42
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-03 23:35:25 -0300

    feat: add aluno in turma

--- src/controller/turma.ts
+++ src/controller/turma.ts
@@ -122,4 +122,100 @@ router.get('/:codigo/media', async (req, res, next) => {
   }
 });
 
+router.post('/:codigo/inscricao', async (req, res, next) => {
+  try {
+    const { matricula } = req.body;
+    const { codigo } = req.params;
+    const codigoTurma = Number.parseInt(codigo);
+
+    const turma = await prisma.turma.findUnique({
+      where: {
+        codigo: codigoTurma,
+      },
+      include: {
+        Disciplina: {
+          include: {
+            preRequisitos: true,
+          },
+        },
+      },
+    });
+
+    const preRequisitos = turma?.Disciplina.preRequisitos;
+    const preRequisitoCodigos = preRequisitos?.map(
+      preRequisito => preRequisito.codigo,
+    );
+
+    const numberOfPreRequisitoApproved = await prisma.avaliacao.count({
+      where: {
+        matriculaAluno: matricula,
+        turma: {
+          disciplinaId: {
+            in: preRequisitoCodigos,
+          },
+        },
+        situacao: Situacao.APROVADO,
+      },
+    });
+
+    const isAlunoApprovedInAllPreRequisito =
+      numberOfPreRequisitoApproved === preRequisitoCodigos?.length;
+
+    const disciplinaId = turma?.Disciplina.codigo;
+
+    const alreadyApprovedInDisciplina = await prisma.avaliacao.findFirst({
+      where: {
+        matriculaAluno: matricula,
+        turma: {
+          Disciplina: {
+            codigo: disciplinaId,
+          },
+        },
+        situacao: {
+          equals: Situacao.APROVADO,
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    const vacanciesInTurma = await prisma.turma.findUnique({
+      where: {
+        codigo: codigoTurma,
+      },
+      select: {
+        qtdVagas: true,
+        Avaliacao: {
+          select: {
+            matriculaAluno: true,
+          },
+        },
+      },
+    });
+
+    const isVacancy =
+      vacanciesInTurma &&
+      vacanciesInTurma.Avaliacao.length < vacanciesInTurma.qtdVagas;
+
+    let alunoInTurma;
+    if (
+      isVacancy &&
+      isAlunoApprovedInAllPreRequisito &&
+      !alreadyApprovedInDisciplina
+    ) {
+      alunoInTurma = await prisma.avaliacao.create({
+        data: {
+          matriculaAluno: matricula,
+          codigoTurma,
+        },
+      });
+    }
+
+    res.json(alunoInTurma);
+  } catch (error) {
+    next(error);
+  }
+});
+
 export default router;
