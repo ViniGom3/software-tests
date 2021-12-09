a88165c770c7d7441a6551c9b8fec12e626ffe52
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-09 09:31:03 -0300

    refactor: move logical from controller to service

--- src/controller/curso/index.ts
+++ src/controller/curso/index.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
-import { Exception } from '../../error';
-import prisma from '../../prisma';
-import { calcularIra } from '../../utils/alunos';
+import {
+  createCurso,
+  deleteCurso,
+  getAllCursos,
+  getIraByCurso,
+  updateCurso,
+} from '../../services/curso';
 
 const router = Router();
 
 router.get('/', async (_, res, next) => {
   try {
-    const allCursos = await prisma.curso.findMany();
-
-    res.json(allCursos);
+    res.json(await getAllCursos());
   } catch (error) {
     next(error);
   }
@@ -17,16 +19,7 @@ router.get('/', async (_, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
-    const { nome, codigo } = req.body;
-
-    const curso = await prisma.curso.create({
-      data: {
-        nome,
-        codigo,
-      },
-    });
-
-    res.json(curso);
+    res.json(await createCurso(req.body));
   } catch (error) {
     next(error);
   }
@@ -34,24 +27,7 @@ router.post('/', async (req, res, next) => {
 
 router.delete('/:id_curso', async (req, res, next) => {
   try {
-    const { id_curso } = req.params;
-    const id = parseInt(id_curso);
-
-    let curso = await prisma.curso.findUnique({
-      where: {
-        id,
-      },
-    });
-
-    if (!curso) throw new Exception(404, 'Curso não encontrado');
-
-    curso = await prisma.curso.delete({
-      where: {
-        id,
-      },
-    });
-
-    res.json(curso);
+    res.json(await deleteCurso(req.params.id_curso));
   } catch (error) {
     next(error);
   }
@@ -59,19 +35,7 @@ router.delete('/:id_curso', async (req, res, next) => {
 
 router.patch('/', async (req, res, next) => {
   try {
-    const { id, nome, codigo } = req.body;
-
-    const curso = await prisma.curso.update({
-      where: {
-        id,
-      },
-      data: {
-        nome,
-        codigo,
-      },
-    });
-
-    res.json(curso);
+    res.json(await updateCurso(req.body));
   } catch (error) {
     next(error);
   }
@@ -79,52 +43,7 @@ router.patch('/', async (req, res, next) => {
 
 router.get('/:id_curso/ira', async (req, res, next) => {
   try {
-    const { id_curso } = req.params;
-    const id = parseInt(id_curso);
-
-    const ultimosPeriodosLetivos = await prisma.periodoLetivo.findMany({
-      orderBy: {
-        dataFim: 'desc',
-      },
-      take: 4,
-      select: {
-        id: true,
-      },
-    });
-
-    const ultimosPeriodosLetivosIds = ultimosPeriodosLetivos.map(
-      periodos => periodos.id,
-    );
-
-    const turmasDoCursoNosUltimosPeriodos = await prisma.avaliacao.findMany({
-      where: {
-        aluno: {
-          cursoId: id,
-        },
-        turma: {
-          periodoLetivoId: {
-            in: ultimosPeriodosLetivosIds,
-          },
-        },
-      },
-      select: {
-        grauFinal: true,
-        situacao: true,
-        turma: {
-          select: {
-            Disciplina: {
-              select: {
-                cargaHoraria: true,
-              },
-            },
-          },
-        },
-      },
-    });
-
-    const IRA = calcularIra(turmasDoCursoNosUltimosPeriodos);
-
-    res.json({ IRA });
+    res.json({ ira: await getIraByCurso(req.params.id_curso) });
   } catch (error) {
     next(error);
   }
---
+++ src/services/curso.ts
@@ -0,0 +1,93 @@
+import { Curso } from '@prisma/client';
+import { Exception } from '../error';
+import prisma from '../prisma';
+import { calcularIra } from '../utils/alunos';
+
+export const getAllCursos = () => {
+  return prisma.curso.findMany();
+};
+
+export const getCursoById = (id: number) => {
+  return prisma.curso.findUnique({
+    where: {
+      codigo: id,
+    },
+  });
+};
+
+export const createCurso = (curso: Curso) => {
+  return prisma.curso.create({
+    data: curso,
+  });
+};
+
+export const deleteCurso = async (id: string) => {
+  const cursoId = parseInt(id);
+
+  if (!(await getCursoById(cursoId)))
+    throw new Exception(404, 'Curso não encontrado');
+
+  return prisma.curso.delete({
+    where: {
+      id: cursoId,
+    },
+  });
+};
+
+export const updateCurso = async (curso: Curso) => {
+  if (!(!!curso.id && (await getCursoById(curso.id))))
+    throw new Exception(404, 'Curso não encontrado');
+
+  return prisma.curso.update({
+    where: {
+      id: curso.id,
+    },
+    data: curso,
+  });
+};
+
+export const getIraByCurso = async (id: string) => {
+  const cursoId = parseInt(id);
+
+  const ultimosPeriodosLetivos = await prisma.periodoLetivo.findMany({
+    orderBy: {
+      dataFim: 'desc',
+    },
+    take: 4,
+    select: {
+      id: true,
+    },
+  });
+
+  const ultimosPeriodosLetivosIds = ultimosPeriodosLetivos.map(
+    periodos => periodos.id,
+  );
+
+  const turmasDoCursoNosUltimosPeriodos = await prisma.avaliacao.findMany({
+    where: {
+      aluno: {
+        cursoId,
+      },
+      turma: {
+        periodoLetivoId: {
+          in: ultimosPeriodosLetivosIds,
+        },
+      },
+    },
+    select: {
+      grauFinal: true,
+      situacao: true,
+      turma: {
+        select: {
+          Disciplina: {
+            select: {
+              cargaHoraria: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return calcularIra(turmasDoCursoNosUltimosPeriodos);
+};
