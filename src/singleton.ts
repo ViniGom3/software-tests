0d0afef4907b9f3e41560db98d8dece0f4540b0b
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-08 22:53:07 -0300

    chore: config ignore singleton mock prisma

--- src/singleton.ts
+++ src/singleton.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended'
 
-import prisma from './client'
+import prisma from './prisma'
 
-jest.mock('./client', () => ({
+jest.mock('./prisma', () => ({
   __esModule: true,
   default: mockDeep<PrismaClient>(),
 }))
