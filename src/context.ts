3e159cdbceb4027fe7d609f29d82d7307d91e794
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-11 14:28:02 -0300

    chore: few remove in context

--- src/context.ts
+++ src/context.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { mockDeep, DeepMockProxy, MockProxy } from 'jest-mock-extended';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 
 export type Context = {
   prisma: PrismaClient;
