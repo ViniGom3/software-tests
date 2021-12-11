68a60ffbb989eac0b71afe2b423dc4a9c935bf42
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-11 13:52:00 -0300

    chore: jest setup

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   clearMocks: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/src/singleton.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/context.ts'],
   testPathIgnorePatterns: ['/node_modules/'],
 };
