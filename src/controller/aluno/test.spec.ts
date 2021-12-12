853a64c3c7e9de32ba994f29a7023c55f4b76963
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-12 12:47:16 -0300

    refactor: use mock in integration test

--- src/controller/aluno/test.spec.ts
+++ src/controller/aluno/test.spec.ts
@@ -1,11 +1,15 @@
 import supertest from 'supertest';
+import { mockPrismaContext as mockPrisma } from '..';
 import { app } from '../..';
 
-// const URI = `http://localhost:${process.env.PORT}`;
-const URI = 'http://localhost:3000';
-
-describe('', () => {
+describe('Test Aluno', () => {
+  beforeAll(() => {
+    process.env.NODE_ENV = 'test';
+  });
+  afterAll(async () => {});
   it('should return 200 and receive empty array', async () => {
+    mockPrisma.prisma.aluno.findMany.mockResolvedValue([]);
+
     const response = await supertest(app).get('/aluno').expect(200);
 
     expect(response.body).toEqual([]);
