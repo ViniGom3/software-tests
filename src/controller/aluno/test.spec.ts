a0dd510398b061b5614502894c6e61a5dbad2ccb
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-12 12:53:32 -0300

    test: ensure return 200 and receive an arry with an aluno

--- src/controller/aluno/test.spec.ts
+++ src/controller/aluno/test.spec.ts
@@ -1,3 +1,4 @@
+import { Aluno } from '@prisma/client';
 import supertest from 'supertest';
 import { mockPrismaContext as mockPrisma } from '..';
 import { app } from '../..';
@@ -6,12 +7,31 @@ describe('Test Aluno', () => {
   beforeAll(() => {
     process.env.NODE_ENV = 'test';
   });
-  afterAll(async () => {});
+
+  afterAll(async () => {
+    jest.clearAllMocks();
+  });
+
   it('should return 200 and receive empty array', async () => {
     mockPrisma.prisma.aluno.findMany.mockResolvedValue([]);
 
     const response = await supertest(app).get('/aluno').expect(200);
 
     expect(response.body).toEqual([]);
+  });
+
+  it('should return 200 and receive an array with an aluno', async () => {
+    const aluno = [
+      {
+        matricula: 0,
+        status: 'ATIVO',
+      },
+    ] as Aluno[];
+
+    mockPrisma.prisma.aluno.findMany.mockResolvedValue(aluno);
+    const response = await supertest(app).get('/aluno').expect(200);
+
+    expect(response.body[0]).toHaveProperty('matricula', 0);
+    expect(response.body[0]).toHaveProperty('status', 'ATIVO');
   });
 });
