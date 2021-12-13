42841257c167d1c327bdbb7d2bfc510e912d8798
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-12-12 21:14:58 -0300

    test: ensure get periodo empty when is not exist periodo

---
+++ src/controller/periodo/test.spec.ts
@@ -0,0 +1,38 @@
+import { PeriodoLetivo } from '@prisma/client';
+import supertest from 'supertest';
+import { mockPrismaContext as mockPrisma } from '..';
+import { app } from '../..';
+
+let periodo: PeriodoLetivo;
+let periodos: PeriodoLetivo[] = [];
+
+describe('Test PeriodoLetivo', () => {
+  beforeAll(() => {
+    process.env.NODE_ENV = 'test';
+
+    periodo = {
+      id: 0,
+      dataInicio: new Date(),
+      dataFim: new Date(),
+      status: 'ATIVO',
+    };
+
+    periodos.push(periodo);
+  });
+
+  beforeEach(() => {
+    mockPrisma.prisma.periodoLetivo.findUnique.mockClear();
+  });
+
+  afterAll(async () => {
+    jest.clearAllMocks();
+  });
+
+  it('should return 200 and receive empty array', async () => {
+    mockPrisma.prisma.periodoLetivo.findMany.mockResolvedValueOnce([]);
+
+    const response = await supertest(app).get('/periodo').expect(200);
+
+    expect(response.body).toEqual([]);
+  });
+});
